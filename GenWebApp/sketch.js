f39f7da8ace9233f0b5c69b9e1dc1b6048d57591
David-Killian <killianda36@gmail.com>
2023-01-30 12:33:05 -0500

    sdf

--- GenWebApp/sketch.js
+++ GenWebApp/sketch.js
@@ -1,13 +1,22 @@
 // Initialize the canvas
+let bgColor; 
+
 function setup() {
-  var canvas = createCanvas(400, 400);
+  var canvas = createCanvas(windowWidth/2.7, windowWidth/2.7);
   noLoop();
   canvas.parent("sketch-holder");
+  bgColor = color(random(255), random(255), random(255));
+  background(bgColor);
 }
 
 // Draw the canvas
 function draw() {
-  background(random(255), random(255), random(255));
+  bgColor = color(random(255), random(255), random(255));
+  background(bgColor)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth/2.7, windowWidth/2.7, false);
 }
 
 
