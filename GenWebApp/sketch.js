497875139b3324a92a07eeba802149f8611eff99
David-Killian <killianda36@gmail.com>
2023-01-30 11:24:43 -0500

    webapp starting

---
+++ GenWebApp/sketch.js
@@ -0,0 +1,13 @@
+// Initialize the canvas
+function setup() {
+  var canvas = createCanvas(400, 400);
+  noLoop();
+  canvas.parent("sketch-holder");
+}
+
+// Draw the canvas
+function draw() {
+  background(random(255), random(255), random(255));
+}
+
+
